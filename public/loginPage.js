409a057e45429c75c990b9b4f2a90c2a8961252a
Konstantin Ozhug <ozkostay@mail.ru>
2022-05-24 16:51:12 +0500

    diplomJS1 - 2 24-05-22

--- public/loginPage.js
+++ public/loginPage.js
@@ -5,8 +5,11 @@ const userForm = new UserForm();
 userForm.loginFormCallback = (data) => {
     console.log(data);
     ApiConnector.login(data, response => {
+        //console.log('log: ', response);
         if (response.success) {
             location.reload();
+        } else {
+            userForm.setLoginErrorMessage(response.error)
         }
     });
 };
@@ -14,7 +17,12 @@ userForm.loginFormCallback = (data) => {
 userForm.registerFormCallback = (data) => {
     console.log(data);
     ApiConnector.register(data, response => {
-        console.log('reg: ', response);
+        //console.log('reg: ', response);
+        if (response.success) {
+            location.reload();
+        } else {
+            userForm.setRegisterErrorMessage(response.error)
+        }
     });
 };
 
