8755d520b6ef907ee8e88881e9196824723d16b2
Konstantin Ozhug <ozkostay@mail.ru>
2022-05-27 15:33:57 +0500

    diplomJS1 - 5 27-05-22

--- public/homePage.js
+++ public/homePage.js
@@ -1,4 +1,5 @@
 
+
 const logoutBatton = new LogoutButton();
 
 // обработка нажатия кнопки выйти
@@ -19,7 +20,6 @@ ApiConnector.current(response => {
 
 // Получение текущих курсов валют
 const rateBoard = new RatesBoard();
-
 const reNewStocks = () => {
     //console.log('Обновили валюты');
     ApiConnector.getStocks(response => {
@@ -32,7 +32,94 @@ const reNewStocks = () => {
 reNewStocks();
 setInterval( reNewStocks, 5000 );
 
-//***************************************************************
-//    Далее всё ещё в разработке
-//**************************************************************
+// Пополнение счета
+const moneyManager = new MoneyManager;
+moneyManager.addMoneyCallback = (data) => {
+    ApiConnector.addMoney(data, response => {
+        let message;
+        if (response.success) {
+            ProfileWidget.showProfile(response.data);
+            message = 'Пополнение счета на '  + data.amount  + ' ' + data.currency;
+        } else {
+            message = 'Ошибка пополнения';
+        }
+        // Сообщения придумал сам, готовых не нашел
+        moneyManager.setMessage(response.success, message) 
+    });
+};
+
+//Конвертация валюты
+moneyManager.conversionMoneyCallback = (data) => {
+    ApiConnector.convertMoney(data, response => {
+        let message;
+        //console.log('sss ', data);
+        if (response.success) {
+            ProfileWidget.showProfile(response.data);
+            message = 'Конвертация '  + data.fromAmount + ' ' + data.fromCurrency  + ' в ' + data.targetCurrency;
+        } else {
+            message = 'Ошибка конвертации';
+        }
+        // Сообщения придумал сам, готовых не нашел
+        moneyManager.setMessage(response.success, message) 
+    });
+};
+
+// Перевод валюты
+moneyManager.sendMoneyCallback = (data) => {
+    ApiConnector.transferMoney( data, response => {
+        let message;
+        //console.log(response);
+        if (response.success) {
+            ProfileWidget.showProfile(response.data);
+            message = 'Перевод '  + data.amount + ' ' + data.currency  + ' для ' + data.to;
+        } else {
+            message = 'Ошибка перевода';
+        }
+        // Сообщения придумал сам, готовых не нашел
+        moneyManager.setMessage(response.success, message) 
+    });
+};
+
+// Заполнение избранного
+const favoritesWidget = new FavoritesWidget();
+ApiConnector.getFavorites( data => {
+    //console.log(data);
+    if (data.success) {
+        favoritesWidget.clearTable();
+        favoritesWidget.fillTable(data.data);
+        moneyManager.updateUsersList(data.data);
+        
+    }
+});
+
+// Добавление в избранное
+favoritesWidget.addUserCallback = data => {
+    ApiConnector.addUserToFavorites(data, response => {
+        if (response.success) {
+            favoritesWidget.clearTable();
+            favoritesWidget.fillTable(response.data);
+            moneyManager.updateUsersList(response.data);
+            message = 'Добавлен пользователь: ID '  + data.id + ' Имя ' + data.name;
+        } else {
+            message = 'Ошибка добавления пользователя: ID '  + data.id + ' Имя ' + data.name;
+        }
+        favoritesWidget.setMessage(response.success, message);
+    });
+}
+
+// Удаление из избранного
+favoritesWidget.removeUserCallback = data => {
+    ApiConnector.removeUserFromFavorites(data, response => {
+        if (response.success) {
+            favoritesWidget.clearTable();
+            favoritesWidget.fillTable(response.data);
+            moneyManager.updateUsersList(response.data);
+            message = 'Удален пользователь: ID= '  + data.id + ' Имя ' + data.name;
+        } else {
+            message = 'Ошибка удаления пользователя: ID= '  + data;
+        }
+        favoritesWidget.setMessage(response.success, message);
+    });
+}
+
 
